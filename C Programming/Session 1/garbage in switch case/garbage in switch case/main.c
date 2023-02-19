7502115e0b332dd4b9b68217a6c26798a22f5302
omaranwar21 <omarahmed11185@gmail.com>
2023-02-19 17:47:51 +0200

    garbage in switch case

---
+++ C Programming/Session 1/garbage in switch case/garbage in switch case/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 29, 2022
+ *      Author: Anwar
+ */
+#include <stdio.h>
+
+int Array[5] = {1,2,3,4,5};
+
+void print (int x){
+	printf("%d\n", x);
+}
+
+int main(int argc, char **argv) {
+	int i = 3;
+
+	switch (i) {
+	int y = 9;
+		case 1:
+			print(1);
+			break;
+		case 3:
+			printf("%d",y);
+			break;
+		default: break;
+	}
+
+}
+
